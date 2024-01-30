697700a9fc3a1c2cf74bab3605f2ecdbedd62c3b
Levan Natchkebia <levan.natchkebia@hotmail.com>
2024-01-30 13:59:45 +0400

    feat: add additional feature in JS

--- scripts/app.js
+++ scripts/app.js
@@ -146,7 +146,10 @@ for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
-    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+    button.addEventListener("click", (e) => {
+        initGame(e.target, String.fromCharCode(i))
+        button.disabled = true;
+    });
 }
 
 document.addEventListener('keydown', (e) => {
