c8e12432119962bbcf880c684a92ba55fe5c7002
Levan Natchkebia <levan.natchkebia@hotmail.com>
2024-01-30 13:53:28 +0400

    feat: change feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,81 @@
-const wordDisplay = document.querySelector(".word-display");
-const guessesText = document.querySelector(".guesses-text b");
-const keyboardDiv = document.querySelector(".keyboard");
-const hangmanImage = document.querySelector(".hangman-box img");
-const modal = document.querySelector(".modal");
-const playAgainBtn = modal.querySelector("button");
+// // Creating HTML elements in DOM 
+const body = document.body;
+
+const modal = document.createElement('div');
+
+modal.classList.add('modal');
+body.appendChild(modal);
+
+const modalContent = document.createElement('div');
+modalContent.classList.add('content');
+modal.appendChild(modalContent);
+
+const modalImage = document.createElement('img');
+modalImage.setAttribute("alt", "GIF");
+modalContent.appendChild(modalImage);
+
+const modalH4 = document.createElement('h4');
+modalContent.appendChild(modalH4);
+
+const modalP = document.createElement('p');
+const pbold = document.createElement('b');
+modalP.appendChild(pbold);
+modalContent.appendChild(modalP);
+
+const playAgainBtn = document.createElement('button');
+playAgainBtn.classList.add('play-again');
+playAgainBtn.innerText = "Play Again";
+modalContent.appendChild(playAgainBtn);
+
+// Create Container
+
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
+
+const hangmanBox = document.createElement('div');
+hangmanBox.classList.add('.hangman-box');
+container.appendChild(hangmanBox);
+
+const hangmanImage = document.createElement('img');
+hangmanBox.appendChild(hangmanImage);
+hangmanImage.setAttribute("alt", "hangman-image");
+
+const boxheader = document.createElement('h1');
+hangmanBox.appendChild(boxheader);
+boxheader.innerText = 'Hangman Game';
+boxheader.setAttribute("alt", "image");
+
+// Create Game Box
+
+const gameBox = document.createElement('div');
+gameBox.classList.add('game-box');
+container.appendChild(gameBox);
+
+const wordDisplay = document.createElement('div');
+wordDisplay.classList.add('word-display');
+gameBox.appendChild(wordDisplay);
+
+const hintH4 = document.createElement('h4');
+hintH4.classList.add('hint-text');
+gameBox.appendChild(hintH4);
+hintH4.innerText = 'Hint: ';
+
+const hintBold = document.createElement('b');
+hintH4.appendChild(hintBold);
+
+const guessesText = document.createElement('h4');
+guessesText.classList.add('guesses-text');
+guessesText.innerText = 'Incorrect guesses: ';
+gameBox.appendChild(guessesText);
+
+const guessBold = document.createElement('b');
+guessesText.appendChild(guessBold);
+
+const keyboardDiv = document.createElement('div');
+keyboardDiv.classList.add('keyboard');
+gameBox.appendChild(keyboardDiv)
+
 
 let currentWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
@@ -12,7 +84,7 @@ const resetGame = () => {
     correctLetters = [];
     wrongGuessCount = 0;
     hangmanImage.src = "./assets/hangman-0.svg";
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    guessBold.innerText = `${wrongGuessCount} / ${maxGuesses}`;
     wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
     keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
     modal.classList.remove("show");
@@ -21,7 +93,7 @@ const resetGame = () => {
 const getRandomWord = () => {
     const { word, hint } = test[Math.floor(Math.random() * test.length)];
     currentWord = word; 
-    document.querySelector(".hint-text b").innerText = hint;
+    hintBold.innerText = hint;
     resetGame();
 }
 
@@ -34,22 +106,40 @@ const gameOver = (isVictory) => {
 }
 
 const initGame = (button, clickedLetter) => {
-    if(currentWord.includes(clickedLetter)) {
-        [...currentWord].forEach((letter, index) => {
-            if(letter === clickedLetter) {
-                correctLetters.push(letter);
-                wordDisplay.querySelectorAll("li")[index].innerText = letter;
-                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
-            }
-        });
-    } else {
-        wrongGuessCount++;
-        hangmanImage.src = `./assets/hangman-${wrongGuessCount}.svg`;
+    if (button) {
+        if(currentWord.includes(clickedLetter)) {
+            [...currentWord].forEach((letter, index) => {
+                if(letter === clickedLetter) {
+                    correctLetters.push(letter);
+                    wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                    wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+                }
+            });
+        } else {
+            wrongGuessCount++;
+            hangmanImage.src = `./assets/hangman-${wrongGuessCount}.svg`;
+        }
+        guessBold.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    
+        if(wrongGuessCount === maxGuesses) return gameOver(false);
+        if(correctLetters.length === currentWord.length) return gameOver(true);
+    }else {
+        if(currentWord.includes(clickedLetter.toLowerCase())) {
+            [...currentWord].forEach((letter, index) => {
+                if(letter === clickedLetter.toLowerCase()) {
+                    correctLetters.push(letter);
+                    wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                    wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+                }
+            });
+        } else {
+            wrongGuessCount++;
+            hangmanImage.src = `./assets/hangman-${wrongGuessCount}.svg`;
+        }
+        if(wrongGuessCount === maxGuesses) return gameOver(false);
+        if(correctLetters.length === currentWord.length) return gameOver(true);
+        guessBold.innerText = `${wrongGuessCount} / ${maxGuesses}`;
     }
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
-
-    if(wrongGuessCount === maxGuesses) return gameOver(false);
-    if(correctLetters.length === currentWord.length) return gameOver(true);
 }
 
 for (let i = 97; i <= 122; i++) {
@@ -58,6 +148,12 @@ for (let i = 97; i <= 122; i++) {
     keyboardDiv.appendChild(button);
     button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
 }
+
+document.addEventListener('keydown', (e) => {
+    if(e.keyCode >= 65 && e.keyCode <=90) {
+        initGame(null, String.fromCharCode(e.keyCode));
+    } 
+});
 
 getRandomWord();
 playAgainBtn.addEventListener("click", getRandomWord);
